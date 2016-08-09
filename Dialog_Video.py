60d4d4fea443afee84489c9880b05758f2280078
CactusTribe <lefrancjoaquim@gmail.com>
2016-08-09 16:26:30 +0200

    Ajout du bouton play capture video

--- Dialog_Video.py
+++ Dialog_Video.py
@@ -12,7 +12,26 @@ class Dialog_Video(QDialog):
 	def __init__(self, parent, client):
 		QDialog.__init__(self, parent)
 		uic.loadUi('Dialog_Video.ui', self)
-		
+
+		self.capture = False
+
+		self.pushButton_capture.clicked.connect(self.captureState)
+		self.pushButton_photo.clicked.connect(self.takePicture)
+
+	def captureState(self):
+		if self.capture == False:
+			self.capture = True
+			buttonIcon = QIcon(QPixmap(":/resources/img/resources/img/Pause-icon.png"))
+			self.pushButton_capture.setIcon(buttonIcon)
+
+		else:
+			self.capture = False
+			buttonIcon = QIcon(QPixmap(":/resources/img/resources/img/Play-icon.png"))
+			self.pushButton_capture.setIcon(buttonIcon)
+
+	def takePicture(self):
+		print("Take capture")
+
 
 	def closeEvent(self, event):
 		print("Stop video")
