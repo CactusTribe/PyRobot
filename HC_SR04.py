353d3de347fd6164677d41499b62c64351d3971f
CactusTribe <lefrancjoaquim@gmail.com>
2016-09-16 17:13:17 +0200

    HC_SR04 en objets

--- HC_SR04.py
+++ HC_SR04.py
@@ -1,61 +1,65 @@
 import time, sys
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+class HC_SR04:
+	def __init__(self, trig_pin, echo_pin):
+		self.trigger = trig_pin
+		self.echo = echo_pin
 
-GPIO_TRIGGER = 19
-GPIO_ECHO = 26
+		GPIO.setup(self.trigger, GPIO.OUT)  # Trigger
+		GPIO.setup(self.echo, GPIO.IN)      # Echo
 
-NB_LOOP = 8
+		# Set trigger to False (Low)
+		GPIO.output(self.trigger, False)
 
-print("Ultrasonic Measurement :")
-
-GPIO.setup(GPIO_TRIGGER,GPIO.OUT)  # Trigger
-GPIO.setup(GPIO_ECHO,GPIO.IN)      # Echo
-
-# Set trigger to False (Low)
-GPIO.output(GPIO_TRIGGER, False)
-
-# Allow module to settle
-time.sleep(0.5)
-
-while True:
-
-	echantillons = []
-
-	for i in range(NB_LOOP):
+	def getDistance(self):
 		# Send 10us pulse to trigger
-		GPIO.output(GPIO_TRIGGER, True)
+		GPIO.output(self.trigger, True)
 		time.sleep(0.00001)
-		GPIO.output(GPIO_TRIGGER, False)
+		GPIO.output(self.trigger, False)
 		start = time.time()
 
-		while GPIO.input(GPIO_ECHO)==0:
+		while GPIO.input(self.echo)==0:
 		  start = time.time()
 
-		while GPIO.input(GPIO_ECHO)==1:
+		while GPIO.input(self.echo)==1:
 		  stop = time.time()
 
 		# Calculate pulse length
 		elapsed = stop-start
-
 		# Distance pulse travelled in that time is time
 		# multiplied by the speed of sound (cm/s)
 		distance = elapsed * 34000
-
 		# That was the distance there and back so halve the value
 		distance = distance / 2
-		echantillons += [distance]
 
-	echantillons.remove(max(echantillons))
-	echantillons.remove(min(echantillons))
+		return distance
 
-	average = sum(echantillons) / len(echantillons)
+	def capture(self):
+		NB_LOOP = 8
 
-	sys.stdout.write("\rDistance : {} cm       ".format(int(average)))
-	sys.stdout.flush()
-	time.sleep(0.2)
+		while True:
+			echantillons = []
 
-# Reset GPIO settings
-GPIO.cleanup()
+			for i in range(NB_LOOP):
+				distance = self.getDistance()
+				echantillons += [distance]
+
+			echantillons.remove(max(echantillons))
+			echantillons.remove(min(echantillons))
+
+			average = sum(echantillons) / len(echantillons)
+
+			sys.stdout.write("\rDistance : {} cm       ".format(int(average)))
+			sys.stdout.flush()
+			time.sleep(0.2)
+
+if __name__ == "__main__":
+
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setwarnings(False)
+
+	HC_SR04 = HC_SR04(19, 26)
+	HC_SR04.capture()
+
+	GPIO.cleanup()
